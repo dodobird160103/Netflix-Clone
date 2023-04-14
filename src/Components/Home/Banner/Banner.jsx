ed88a61dce2fd64caca090d3ab9dd6b5b92e2f28
dodobird160103 <farhan14711@gmail.com>
2023-04-15 00:46:16 +0500

    Trailer to be added

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import './Banner.css';
+import {AiOutlinePlayCircle} from 'react-icons/ai';
+import {AiOutlinePlus} from 'react-icons/ai';
 
 export const Banner = ({fetchURL}) => {
     /* Background image
@@ -17,7 +19,7 @@ export const Banner = ({fetchURL}) => {
     useEffect(() => {
         async function imgData() {
             const request = await axios.get(`${BaseURL}${fetchURL}`);
-            // Get random Banner Image from results array 
+            // Get random movie from results array 
             setImg(request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1)
             ]); 
@@ -39,11 +41,11 @@ export const Banner = ({fetchURL}) => {
             <div className="banner__contents">
                 {/*<img src={`${imgURL}${Img.poster_path}`} alt={Img.name} />*/}
 
-                <h1 >{Img?.title || Img?.name || Img?.original_name}</h1>
+                <h1 className="banner__title" >{Img?.title || Img?.name || Img?.original_name}</h1>
 
                 <div className="banner__buttons" >
-                    <buttons className="banner__button">Play</buttons>
-                    <buttons className="banner__button">My List</buttons>
+                    <buttons className="banner__button"><AiOutlinePlayCircle /><p >Play</p></buttons>
+                    <buttons className="banner__button"><AiOutlinePlus /><p >My List</p></buttons>
                 </div>
 
                 <h4 className="description">
