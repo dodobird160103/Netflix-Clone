e92aaa8067bbcbde84c395c0fd70d857ae20d9b5
dodobird160103 <farhan14711@gmail.com>
2023-04-17 17:04:18 +0500

    Finalized

--- src/Components/Home/Row/Row.jsx
+++ src/Components/Home/Row/Row.jsx
@@ -11,7 +11,8 @@ export const Row = ({title, fetchURL}) => {
 
     const BaseURL = "https://api.themoviedb.org/3";
     const imgURL = "https://image.tmdb.org/t/p/original";
-   
+    const API_KEY = "87b91e596b93c0a797945593f9a30864";
+    const Youtube_URL = "https://www.youtube.com/watch?v=";
 
     useEffect(() => {
         async function fetchData() {
@@ -31,9 +32,10 @@ export const Row = ({title, fetchURL}) => {
         height: "390",
         width: "100%",
         playerVars: {
-            autoplay: 1
+            autoplay: 1,
         },
     };
+
 
     const handleClick = (item) => {
         if(trailerURL)
@@ -42,12 +44,24 @@ export const Row = ({title, fetchURL}) => {
         }
         else
         {
-            movieTrailer(item.name || "").then(url => {
-                const urlParams = new URLSearchParams (new URL(url).search);
-                setTrailerUrl(urlParams.get('v'));
-                console.log(setTrailerUrl(urlParams.get('v')));
-            }).catch(error => console.log(error));
             
+            movieTrailer(item?.name || "")
+            .then((url) => {
+                console.log(url);
+                const urlParams =  new URLSearchParams (new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+                console.log(setTrailerUrl(urlParams.get('v')));
+            }).catch((error) => console.log(error));
+            
+            
+            /*
+            const request = await axios.get(`${BaseURL}/movie/${item.id}/videos?api_key=${API_KEY}&language=en-US`);
+            console.log(`${BaseURL}/movie/${item.id}/videos?api_key=${API_KEY}&language=en-US`);
+            console.log(request);
+            const Key_ID = request.data.results[0].key;
+            console.log(Key_ID);
+            setTrailerUrl(`${Youtube}${Key_ID}`);
+            console.log(trailerURL);*/
         }
     }
 
